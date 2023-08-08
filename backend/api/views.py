de0e70225bf1c8801aab97394e34d0d498510654
Саша Рыбенков <rybenkov01@mail.ru>
2023-08-08 17:30:23 +0300

    Views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,15 @@
+from http import HTTPStatus
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, filters, generics, mixins
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes, action
+from rest_framework.response import Response
+from .pagination import CustomPaginator
 
 from users.models import Subscribe, User
+from recipes.models import Tag, Ingredient, Recipes, Favorites, Shopping_list
 from .permissions import IsOwnerOrReadOnly
 from .pagination import CustomPaginator
 from .serializers import (
@@ -19,4 +24,154 @@ from .serializers import (
 class UserViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
                   mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.all()
-    permission_classes = (AllowAny,)
+    permission_classes = [AllowAny]
+    serializer_class = UserListSerializer
+    pagination_class = CustomPaginator
+
+    @action(
+        methods=[
+            'GET',
+            'PATCH',
+        ],
+        permission_classes=[IsAuthenticated],
+        detail=False,
+        url_path='me',
+    )
+    def users_profile(self, request):
+        serializer = UserListSerializer(request.user)
+        return Response(serializer.data, status=HTTPStatus.OK)
+    
+    @action(
+        methods=['POST'],
+        permission_classes=[IsAuthenticated],
+        detail=False,
+    )
+    def users_password(self, request):
+        serializer = ChangePasswordSerializer(request.user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'detail': 'Пароль изменен!'}, status=HTTPStatus.NO_CONTENT)
+        
+    @action(
+        methods=['GET'],
+        permission_classes =[IsAuthenticated],
+        detail=False,
+        pagination_class = CustomPaginator
+    )
+    def subscriptions(self, request):
+        serializer = SubscribesSerializer(request.user)
+        return self.get_paginated_response(serializer.data)
+    
+    @action(
+        methods=[
+            'POST',
+            'DELETE',
+        ],
+        permission_classes =[IsAuthenticated],
+        detail=False,
+    )
+    def subsscribe(self, request, **kwargs):
+        author = get_object_or_404(User, id=kwargs['pk'])
+
+        if request.method == 'POST':
+            serializer = SubscribeAuthorSerializer(
+                author, data=request.data, context={"request": request})
+            serializer.is_valid(raise_exception=True)
+            Subscribe.objects.create(user=request.user, author=author)
+            return Response(serializer.data, status=HTTPStatus.CREATED)
+
+        if request.method == 'DELETE':
+            get_object_or_404(Subscribe, user=request.user,
+                              author=author).delete()
+            return Response({'detail': 'Удалено'},
+                            status=HTTPStatus.NO_CONTENT)
+
+
+class TagViewSet(mixins.ListModelMixin,
+          mixins.RetrieveModelMixin,
+          viewsets.GenericViewSet):
+    queryset = Tag.object.all()
+    serializer_class = TagSerializer
+    permission_classes = [AllowAny]
+
+
+class IngredientViewSet(mixins.ListModelMixin,
+                 mixins.RetrieveModelMixin,
+                 viewsets.GenericViewSet):
+    queryset = Ingredient.object.all()
+    serializer_class = IngredientSerializer
+    permission_classes = [AllowAny]
+    search_fields = ('^name', )
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipes.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = [AllowAny]
+    pagination_class = CustomPaginator
+    http_method_names = ['get', 'post', 'patch', 'create', 'delete']
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return RecipeSerializer
+        return RecipeCreateSerializer
+    
+    @action(
+        methods=[
+            'POST',
+            'DELETE',
+        ],
+        permission_classes=[IsAuthenticated],
+        detail=True
+    )
+    def favorites(self, request, **kwargs):
+        recipe = get_object_or_404(Recipes, id=kwargs['pk'])
+
+        if request.method == 'POST':
+            serializer = RecipeSerializer(recipe, data=request.data,
+                                          context={"request": request})
+            serializer.is_valid(raise_exception=True)
+            if not Favorites.objects.filter(user=request.user,
+                                           recipe=recipe).exists():
+                Favorites.objects.create(user=request.user, recipe=recipe)
+                return Response(serializer.data,
+                                status=HTTPStatus.CREATED)
+            return Response({'errors': 'Рецепт уже в избранном.'},
+                            status=HTTPStatus.BAD_REQUEST)
+
+        if request.method == 'DELETE':
+            get_object_or_404(Favorites, user=request.user,
+                              recipe=recipe).delete()
+            return Response({'detail': 'Рецепт успешно удален из избранного.'},
+                            status=HTTPStatus.NO_CONTENT)
+        
+    @action(
+        methods=[
+            'POST',
+            'DELETE',
+        ],
+        permission_classes=[IsAuthenticated],
+        detail=True
+    )
+    def shopping_list(self, request, **kwargs):
+        recipe = get_object_or_404(Recipes, id=kwargs['pk'])
+
+        if request.method == 'POST':
+            serializer = RecipeSerializer(recipe, data=request.data,
+                                          context={"request": request})
+            serializer.is_valid(raise_exception=True)
+            if not .objects.filter(user=request.user,
+                                                recipe=recipe).exists():
+                Shopping_cart.objects.create(user=request.user, recipe=recipe)
+                return Response(serializer.data,
+                                status=status.HTTP_201_CREATED)
+            return Response({'errors': 'Рецепт уже в списке покупок.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if request.method == 'DELETE':
+            get_object_or_404(Shopping_cart, user=request.user,
+                              recipe=recipe).delete()
+            return Response(
+                {'detail': 'Рецепт успешно удален из списка покупок.'},
+                status=status.HTTP_204_NO_CONTENT
+            )
--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -114,6 +114,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': [
+        'api.pagination.CustomPaginator',
+    ],
+    'PAGE_SIZE': 6,
+    'SEARCH_PARAM': 'name',
 }
 
 DJOSER = {
